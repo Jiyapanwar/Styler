9e5a5fb020772db1958da57d975c771a0aba4308
Jiyapanwar <jiyapanwar2424@gmail.com>
2025-09-23 17:17:36 +0530

    .

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import CircularGallery from "./CircularGallery";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Landing = () => {
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const container = containerRef.current;
+
+    gsap.to(container, {
+      //   scrollTrigger: {
+      //     trigger: container,
+      //     // start: "top top",
+      //     end: "+=2000",
+      //     pin: true,
+      //     scrub: 0.1,
+      //   },
+    });
+  }, []);
+
   return (
-    <div className="flex items-center justify-center h-[80vh] ">
+    <div className="flex flex-col items-center justify-center h-[80vh] ">
       <h1 className="font-inter  text-[370px] font-semibold text-[#191919]">
         Styler
       </h1>
-      <div style={{ height: "600px", position: "relative" }}>
-        4{" "}
+      <div ref={containerRef} className="w-full  ">
         <CircularGallery
-          bend={3}
+          bend={15}
           textColor="#ffffff"
-          borderRadius={0.05}
+          borderRadius={0.25}
           scrollEase={0.02}
         />
-        5
       </div>
     </div>
   );
