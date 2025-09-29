6af9c0d791380b189e653f9cae6ddf326b4c4a7b
Jiyapanwar <jiyapanwar2424@gmail.com>
2025-09-29 16:37:58 +0530

    routing  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import AnimatedText from "./components/AnimatedText";
 import InfiniteImageGrid from "./components/InfiniteImageGrid";
 import Footer from "./components/Footer";
-
-// import SecondSection from "./components/SecondSection";
+import About from "./components/About";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Landing />
-      <AnimatedText />
-      <InfiniteImageGrid />
-      <Footer />
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Landing />
+              <AnimatedText />
+              <InfiniteImageGrid />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
